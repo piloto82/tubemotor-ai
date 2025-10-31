1a52f7117ab532e24fd37ed73d908e01f2d2380a
Gilles Maciel lopes <gillesmaciellopes@Mac.Home>
2025-10-31 19:13:31 +0000

    Fix: Atualiza todos os arquivos para a versão corrigida

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    // This makes the environment variable available in the browser-side code
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
+  }
 })
